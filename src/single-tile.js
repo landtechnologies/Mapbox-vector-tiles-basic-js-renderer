200fd5d75e7d0c5fa45aa9815792988cb5d24bf9
Daniel Manson <danielmanson.uk@gmail.com>
2017-03-28 13:41:02 +0100

    more drafting

--- src/single-tile.js
+++ src/single-tile.js
@@ -1,48 +1,71 @@
 const Transform = require('./geo/transform');
 const Painter = require('./render/painter');
+const Style = require('./style/style');
+const Camera = require('./ui/camera');
 
-function MapboxSingleTile(options) {
-  options = options || {};
-  this._transform = new Transform(options.minZoom, options.maxZoom, options.renderWorldCopies);
-  this._canvas = document.createElement('canvas');
-  this._canvas.width = 512;
-  this._canvas.height = 512;
-  this._initOptions = options;
-  this._canvas.addEventListener('webglcontextlost', () => console.log("webglcontextlost"), false);
-  this._canvas.addEventListener('webglcontextrestored', () => this._createGlContext(), false); 
-  this._createGlContext();
-}
+const SIZE = 512;
 
+class MapboxSingleTile extends Camera {
 
-MapboxSingleTile.prototype.showCanvasForDebug = function(){
-  document.body.appendChild(this._canvas);
-  this._canvas.style.position = "fixed";
-  this._canvas.style.top = "20px";
-  this._canvas.style.right = "20px";
-  this._canvas.style.border = "1px solid red";
-}
+  constructor(options) {
+    var transform =  new Transform(options.minZoom, options.maxZoom, options.renderWorldCopies);
+    options = options || {};
+    super(transform, options);  
+    this._transform = transform;
+    this._initOptions = options;
+    this._style = new Style(options.style, this);
+    this._style.setEventedParent(this, {style: this._style});
+    this._canvas = document.createElement('canvas');
+    this._canvas.width = SIZE;
+    this._canvas.height = SIZE;
 
-MapboxSingleTile.prototype._createGlContext = function(){
-  const attributes = Object.assign({
-      failIfMajorPerformanceCaveat: this._initOptions.failIfMajorPerformanceCaveat,
-      preserveDrawingBuffer: this._initOptions.preserveDrawingBuffer
-  }, require('mapbox-gl-supported').webGLContextAttributes);
-  
-  this._gl = this._canvas.getContext('webgl', attributes) ||
-             this._canvas.getContext('experimental-webgl', attributes);
-  if (!this._gl) {
-    throw new Error('Failed to initialize WebGL');
+    this._transform.resize(SIZE, SIZE);
+    this._canvas.addEventListener('webglcontextlost', () => console.log("webglcontextlost"), false);
+    this._canvas.addEventListener('webglcontextrestored', () => this._createGlContext(), false); 
+    this._createGlContext();
   }
-  this._painter = new Painter(this._gl, this._transform);
-}
+
+  showCanvasForDebug(){
+    document.body.appendChild(this._canvas);
+    this._canvas.style.position = "fixed";
+    this._canvas.style.top = "20px";
+    this._canvas.style.right = "20px";
+    this._canvas.style.border = "1px solid red";
+    this._canvas.style.background = "#666";
+  }
+
+  _createGlContext(){
+    const attributes = Object.assign({
+        failIfMajorPerformanceCaveat: this._initOptions.failIfMajorPerformanceCaveat,
+        preserveDrawingBuffer: this._initOptions.preserveDrawingBuffer
+    }, require('mapbox-gl-supported').webGLContextAttributes);
+    
+    this._gl = this._canvas.getContext('webgl', attributes) ||
+               this._canvas.getContext('experimental-webgl', attributes);
+    if (!this._gl) {
+      throw new Error('Failed to initialize WebGL');
+    }
+    this._painter = new Painter(this._gl, this._transform);
+    this._painter.resize(SIZE, SIZE);
+  }
 
 
-MapboxSingleTile.prototype.renderTile = function(z, x, y, options, cb){
-  this._painter.render(this.style, {
-    showTileBoundaries: this._initOptions.showTileBoundaries,
-    showOverdrawInspector: this._initOptions.showOverdrawInspector
-  });
-  cb(null);
+  renderTile(z, x, y, options, cb){
+    // TODO: use x and y properly...
+    this.jumpTo({
+      center: m.map.getCenter().toJSON(),  //{Lat:, Lng: }
+      zoom: z || 15
+    });
+
+    this._style._updateSources(this._transform);
+
+    this._painter.render(this._style, {
+      showTileBoundaries: this._initOptions.showTileBoundaries,
+      showOverdrawInspector: this._initOptions.showOverdrawInspector
+    });
+    cb && cb(null);
+  }
+
 }
 
 export default MapboxSingleTile;

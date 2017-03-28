cc6750d35bfe49a980609c2e707727f225bbfd8b
Daniel Manson <danielmanson.uk@gmail.com>
2017-03-28 12:30:18 +0100

    fix for webpack build

--- src/util/browser/web_worker.js
+++ src/util/browser/web_worker.js
@@ -2,7 +2,7 @@
 
 const WebWorkify = require('webworkify');
 const window = require('../window');
-const workerURL = window.URL.createObjectURL(new WebWorkify(require('../../source/worker'), {bare: true}));
+const workerURL = window.URL.createObjectURL(new WebWorkify(require.resolve('../../source/worker'), {bare: true}));
 
 module.exports = function () {
     return new window.Worker(workerURL);

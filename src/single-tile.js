a8bfee702b14db9f5498ea774f9056905f9d688c
Daniel Manson <danielmanson.uk@gmail.com>
2017-03-29 10:03:50 +0100

    can render something for single tile

--- src/single-tile.js
+++ src/single-tile.js
@@ -1,10 +1,23 @@
-const Transform = require('./geo/transform');
-const Painter = require('./render/painter');
-const Style = require('./style/style');
-const Camera = require('./ui/camera');
-const TileCoord = require('./source/tile_coord');
+const Transform = require('./geo/transform'),
+      Painter = require('./render/painter'),
+      Style = require('./style/style'),
+      Camera = require('./ui/camera'),
+      TileCoord = require('./source/tile_coord'),
+      EXTENT = require('./data/extent'),
+      glmatrix = require('@mapbox/gl-matrix');
+
+const mat4 = glmatrix.mat4;
 
 const SIZE = 512;
+
+window.mat4 = mat4;
+
+mat4.str = function(a) { // better than version in gl-matrix src..and don't need to worry about minification
+    return 'mat4('     + a.slice(0,4).join("  ") + 
+            "\n      " + a.slice(4,8).join("  ") + 
+            "\n      " + a.slice(8,12).join("  ") +
+            "\n      " + a.slice(12,16).join("  ") + ')';
+}
 
 class MapboxSingleTile extends Camera {
 
@@ -12,6 +25,7 @@ class MapboxSingleTile extends Camera {
     var transform =  new Transform(options.minZoom, options.maxZoom, options.renderWorldCopies);
     options = options || {};
     super(transform, options);  
+    transform.calculatePosMatrix = this._transform_calculatePosMatrix.bind(transform);
     this._transform = transform;
     this._initOptions = options;
     this._style = new Style(options.style, this);
@@ -26,6 +40,15 @@ class MapboxSingleTile extends Camera {
     this._createGlContext();
     this._sourceCaches = this._style.sourceCaches
 
+  }
+
+  _transform_calculatePosMatrix(tileCoord, maxZoom) {
+    // override for this._transform.calculatePosMatrix (patched in constructor, above)
+    const S = 4092; // I think this is the size of the tile in its own coordiante scheme, not sure why we have to x2
+    const posMatrix = mat4.identity(new Float64Array(16));
+    mat4.scale(posMatrix, posMatrix, [1.8/(2*S),-1.8/(2*S),1]); // TODO: change 1.8 to 2
+    mat4.translate(posMatrix, posMatrix, [-S,-S,0]);
+    return new Float32Array(posMatrix);
   }
 
   showCanvasForDebug(){
@@ -62,18 +85,13 @@ class MapboxSingleTile extends Camera {
       zoom: z || 15
     });
 
+    // TODO: remove  old tiles from cache
     for(var k in this._sourceCaches){
       this._sourceCaches[k]._coveredTiles = {};
       this._sourceCaches[k].transform = this._transform;
       this._sourceCaches[k].addTile(new TileCoord(z,x,y,0));
     }
-    /*
-    var tileKeys = Object.keys(sourceCache._tiles);
-    for(var k in tileKeys){
-      sourceCache.removeTile(k);
-    }
-    */
-
+    
     this.painter.render(this._style, {
       showTileBoundaries: this._initOptions.showTileBoundaries,
       showOverdrawInspector: this._initOptions.showOverdrawInspector

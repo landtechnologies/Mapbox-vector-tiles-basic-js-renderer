f3c54fe814a921f254dfb6333561ce6356ce7355
Daniel Manson <danielmanson.uk@gmail.com>
2017-04-04 17:21:54 +0100

    single source, support changing styles

--- src/single-tile.js
+++ src/single-tile.js
@@ -2,7 +2,7 @@
   This is basically a plugin for mapbox-gl.
   Asside from the constructor, it provides one main method:
 
-  renderTile(z, x, y, options, callback)
+  renderTile(z, x, y, callback)
   
   The callback is provided with (err, canvas) where the canvas has
   the given tile rendered to it, filling the entire canvas.
@@ -10,21 +10,23 @@
   drawImage onto a new canvas, as there is no guarantee of
   the lifetime of the image on the canvas.
 
-  The options provided to render tile have the following optional fields:
-    size - the width/height in pixels of the rendered result. This is
-           needed for customizing resolution of rendering..it's best to
-           ask for it rendered at the resolution you are going to use it at,
-           though you could do it smaller/larger if you really want.
+  The width (=height) of the tile rendered is set using the .setResolution method.
+  The style for each layer is set as the "style" field passed in the options
+  object to the constructor, but it can be overriden using the .setPaintProperty
+  method.  IMPORTANT: the resolution and style used for rendering is read at the 
+  point the render is actually performed, not at the point it is requested (
+  the render is performed in an async manner, remember). Hopefully this meets 
+  the requirements of the user.
 
   ==========================================
   Notes on development:
 
   At the point renderTile is called, the given tile can be in one of several states:
       1. Never previously mentioned (or long forgotten about).
-         So need to load data from source(s) before rendering.
+         So need to load data from source(s) before rendering. (Browser may
+         have the raw files in its cache, so this may not require making a roundtrip
+         to the server, but that optimization is transparent to us).
       2. Recently requested, but not ready yet (i.e. in _pendingRenders)
-        a. Requested with the same options as this request
-        b. Requested with different options to this request
       3. Recently rendered, with resulting image cached for the correct options.
       4. Previously requested, but image not cached (with correct options).  However
         data is still available, so no need to wait for data before rendering.
@@ -32,10 +34,9 @@
   The property _renderedTileCache is a Least-Recently-Used cache that maps from
   <coord.id + JSON.stringify(options)> to single canvases.
 
-  The property _pendingRenders maps from <coord.id> to an object that contains a "variants" field, which
-  is a map from <JSON.stringify(options)> to options/callbacks.  Note this is not a cache; things are
-  removed from the _pendingRenders only when the rendering is completed (at which point they are added
-  to _renderedTileCache).
+  The property _pendingRenders maps from <coord.id> to an object that contains callbacks.
+  Note this is not a cache: things are removed from the _pendingRenders when the rendering
+  is completed (at which point they are added to _renderedTileCache).
 
 */
 
@@ -47,39 +48,42 @@ const Painter = require('./render/painter'),
       mat4 = require('@mapbox/gl-matrix').mat4,
       Cache = require('./util/lru_cache');
 
-const DEFAULT_SIZE = 256;
+const DEFAULT_RESOLUTION = 256;
 const DEFAULT_CACHE_SIZE = 6;
-const TILE_LOAD_TIMEOUT = 15000;
+const TILE_LOAD_TIMEOUT = 60000;
 
 class MapboxSingleTile extends Evented {
 
   constructor(options) {
     super();
-    options = options || {}; 
-    this.transform = {zoom: 0, angle: 0, pitch: 0};
+    this._initOptions = options = options || {}; 
+    this.transform = {zoom: 0, angle: 0, pitch: 0, scaleZoom: ()=> 0};
     this._posMatrix = this._calculatePosMatrix(); // doesn't depend on anything!
-    this._initOptions = options;
-    this._style = new Style(options.style, this);
+    this._style = new Style(Object.assign({}, options.style, {transition: {duration: 0}}), this);
     this._style.setEventedParent(this, {style: this._style});
+    this._style.on('data', e => (e.dataType === "source") && this._initSourceCache());
+    this._style.on('data', e => (e.dataType === "style") && this._style.update([], {transition: false}));
+    
     this._canvas = document.createElement('canvas');
     this._canvas.addEventListener('webglcontextlost', () => console.log("webglcontextlost"), false);
     this._canvas.addEventListener('webglcontextrestored', () => this._createGlContext(), false); 
     this._createGlContext();
-    this._setSize(DEFAULT_SIZE);
-    this._sourceCaches = this._style.sourceCaches
+    this.setResolution(DEFAULT_RESOLUTION);
+    this._styleOverrides = {};
     this._pendingRenders = {};
     (options.cacheSize !== 0) && (this._renderedTileCache = new Cache(options.cacheSize || DEFAULT_CACHE_SIZE, ()=>{}));
   }
 
-  _setSize(s){
-    if(s == this._size){
-      return;
+  _initSourceCache(){
+    var sources = Object.keys(this._style.sourceCaches);
+    if(sources.length !== 1){
+      throw "expected exactly 1 source"; // could implement multi-source, but not needed yet
     }
-    this._size = s;
-    this._canvas.width = s;
-    this._canvas.height = s;
-    this.transform.pixelsToGLUnits = [2 / s, -2 / s];
-    this.painter.resize(s, s); 
+    this._sourceCache = this._style.sourceCaches[sources[0]]; 
+    this._sourceCache._coveredTiles = {};
+    this._sourceCache.transform = this.transform;
+    this._sourceCache.on('data', e => e.coord && this._renderTileNowDataIsAvailable(e));
+    this._sourceCache.on('error', e => e.tile &&  this._renderTileDataFetchFailed(e));
   }
 
   _calculatePosMatrix() {
@@ -105,19 +109,31 @@ class MapboxSingleTile extends Evented {
     this.painter.style = this._style;
   }
 
-  applyStyles(z) {
-    // alternative to style.js@_recalculate
-    for (const sourceId in this._sourceCaches){
-      this._sourceCaches[sourceId].used = false;
+  setPaintProperty(layer, prop, val){
+    if(val === undefined || val === null){
+      throw "reseting style not implemented";
+    } 
+    this._styleOverrides[layer + "." + prop] = val;
+    this._style.setPaintProperty(layer, prop, val);
+    this._style.update([], {transition: false});
+  }
+
+  _getCurrentVariantString(){
+    return this._resolution + "|" + JSON.stringify(this._styleOverrides);
+  }
+
+  setResolution(r){
+    // resolution at which the tile is rendered,
+    // r is the width (=height) of the rendered tile.
+    if(r == this._resolution){
+      return;
     }
-    for(var layerId of this._style._order){
-      const layer = this._style._layers[layerId];
-      layer.recalculate(z);
-      if (!layer.isHidden(z) && layer.source) {
-        this._sourceCaches[layer.source].used = true;
-      }
-    }
-    this._style._applyClasses([], {transition: false});
+    this._size = r;
+    this._canvas.width = r;
+    this._canvas.height = r;
+    this.transform.pixelsToGLUnits = [2 / r, -2 / r];
+    this.painter.resize(r, r); 
+    this._resolution = r;
   }
 
   _renderTileDataFetchFailed(e){
@@ -139,133 +155,84 @@ class MapboxSingleTile extends Evented {
     var state = this._pendingRenders[e.coord.id];
     if(!state){
       return; // timeout already occured
-    }
-    if(--state.awaitingSources > 0){
+    } else if(--state.awaitingSources > 0){
       return;
+    } else {
+      clearTimeout(state.timeout);
+      delete this._pendingRenders[e.coord.id];      
     }
-    clearTimeout(state.timeout);
-    delete this._pendingRenders[e.coord.id];
 
     var z = e.coord.z;
     this.transform.zoom = z;
-    this.applyStyles(z); // TODO: only do this if zoom has changed      
-
-    for (var variantKey in state.variants){
-      var options = state.variants[variantKey].options;
-      var callbacks = state.variants[variantKey].callbacks;
-      var size = options.size || DEFAULT_SIZE;
-      e.coord.posMatrix = this._posMatrix;
-      for(var k in this._sourceCaches){
-        this._sourceCaches[k].getVisibleCoordinates = () => [e.coord];
-        for(var id in this._sourceCaches[k]._tiles){
-           this._sourceCaches[k]._tiles[id].tileSize = size;
-        }
-      }
-      this._setSize(size);
-
-      this.painter.render(this._style, {
-        showTileBoundaries: this._initOptions.showTileBoundaries,
-        showOverdrawInspector: this._initOptions.showOverdrawInspector
-      });
-      
-      // copy the canvas into cache (if required)
-      var returnCanvas;
-      if(this._renderedTileCache){
-        returnCanvas = document.createElement('canvas');
-        returnCanvas.width = size;
-        returnCanvas.height = size;
-        console.time("drawImage " + e.coord.id + variantKey);
-        returnCanvas.getContext('2d').drawImage(this._canvas, 0, 0);
-        console.timeEnd("drawImage " + e.coord.id + variantKey);
-        this._renderedTileCache.add(e.coord.id + variantKey, returnCanvas);        
-      } else {
-        returnCanvas = this._canvas
-      }
-
-      // return a reference to the cached/main canvas
-      // note that recipient must make use of it immediately,
-      // i.e. by calling drawImage to a new canvas.
-      while(callbacks.length){
-        callbacks.shift()(null, returnCanvas);
-      }
-
+    e.tile.tileSize = this._resolution;
+    e.coord.posMatrix = this._posMatrix;
+    this._sourceCache.getVisibleCoordinates = () => [e.coord];
+    this.painter.render(this._style, {
+      showTileBoundaries: this._initOptions.showTileBoundaries,
+      showOverdrawInspector: this._initOptions.showOverdrawInspector
+    });
+    
+    // copy the canvas into cache (if required)
+    var returnCanvas;
+    if(this._renderedTileCache){
+      returnCanvas = document.createElement('canvas');
+      returnCanvas.width = this._resolution;
+      returnCanvas.height = this._resolution;
+      console.time("drawImage " + e.coord.id + this._getCurrentVariantString());
+      returnCanvas.getContext('2d').drawImage(this._canvas, 0, 0);
+      console.timeEnd("drawImage " + e.coord.id + this._getCurrentVariantString());
+      this._renderedTileCache.add(e.coord.id + this._getCurrentVariantString(), returnCanvas);        
+    } else {
+      returnCanvas = this._canvas
     }
+
+    // return a reference to the cached/main canvas
+    // note that recipient must make use of it immediately,
+    // i.e. by calling drawImage to a new canvas.
+    while(state.callbacks.length){
+      state.callbacks.shift()(null, returnCanvas);
+    }
+
   }
   
-  _initSourcesCaches(){
-    if(this._initSourcesCachesDone){
-      return;
-    }
-    for(var k in this._sourceCaches){
-      this._sourceCaches[k]._coveredTiles = {};
-      this._sourceCaches[k].transform = this.transform;
-      this._sourceCaches[k].on('data', this._renderTileNowDataIsAvailable.bind(this));
-      this._sourceCaches[k].on('error', this._renderTileDataFetchFailed.bind(this));
-    }
-    this._initSourcesCachesDone = true;
-  }
 
-  renderTile(z, x, y, options, next){
-    // see note at top of file for explanaiton of the 4 "states" a tile can be in
-
-    this._initSourcesCaches();
-    
+  renderTile(z, x, y, next){
+    // see note at top of file for explanaiton of the 4 "states" a tile can be in   
     var coord = new TileCoord(z, x, y, 0);    
-    options = options || {};
-    var variantKey = JSON.stringify(options);
     
     // Deal with state (3).
-    if(this._renderedTileCache && this._renderedTileCache.has(coord.id + variantKey)){
-      return next(null, this._renderedTileCache.get(coord.id + variantKey));
+    var cached = this._renderedTileCache && this._renderedTileCache.get(coord.id +  this._getCurrentVariantString());
+    if(cached){
+      return next(null, cached);
     }
 
     // Deal with state (2).
     if(this._pendingRenders[coord.id]){
-      if(this._pendingRenders[coord.id].variants[variantKey]){
-        this._pendingRenders[coord.id].variants[variantKey].callbacks.push(next);
-      } else {
-        this._pendingRenders[coord.id].variants[variantKey] = {
-          options: options,
-          callbacks: [next]
-        }
-      }
+      this._pendingRenders[coord.id].callbacks.push(next);
       return;
     }
 
-
+    var tile = this._sourceCache.addTile(coord);  
     var state = this._pendingRenders[coord.id] = {
-      awaitingSources: 0,
-      variants: {}
-    };
-    state.variants[variantKey] = {
-      options: Object.assign({}, options), // should just be a shallow object, so simply clone is ok
+      awaitingSources: tile.hasData() ? 0 : 1, // state (1) is dealt with when tile.hasData() is false - see .addTile call above
       callbacks: [next],
       timeout: 0
-    };
-
-    for(var k in this._sourceCaches){
-      var tile = this._sourceCaches[k].addTile(coord); 
-      !tile.hasData() && state.awaitingSources++;
-      // if tile.hasData() is false, the .addTile call above will ultimately lead to state (1) being dealt with.
-    }
-
+    };   
+ 
     if(state.awaitingSources == 0){
       // Deal with state (4).
-      setTimeout(() => this._renderTileNowDataIsAvailable({coord: coord}), 1);
+      setTimeout(() => this._renderTileNowDataIsAvailable({coord: coord, tile: tile}), 1);
+
     } else {
+      // More stuff for state (1)..this state is also mentioned a few lines above.
       state.timeout = setTimeout(() => {
         delete this._pendingRenders[coord.id];
-        for(var variantKey in state.variants){
-          var callbacks = state.variants[variantKey].callbacks;
-          while(callbacks.length){
-            callbacks.shift()("fetch timedout");
-          }
+        while(state.callbacks.length){
+          state.callbacks.shift()("fetch timedout");
         }
       }, TILE_LOAD_TIMEOUT);
-
     }
-
-    
+ 
   }
 
   showCanvasForDebug(){
